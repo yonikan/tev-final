7356b843a8452437c78f8d21acaec9fe7cdaa5ae
yoni kangun <yoni.kangun@playermaker.com>
2019-09-02 13:53:36 +0300

    added login, teams and their routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
+import { LoginComponent } from './core/auth/login/login.component';
+import { TeamsComponent } from './core/teams/teams.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'teams', component: TeamsComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamsComponent } from './teams/teams.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TeamsComponent],
   imports: [
     CommonModule
   ]
