a57ad62352bffa2237ca7fdcdbf61971a1cae7f9
yoni kangun <yoni.kangun@playermaker.com>
2019-09-03 09:21:08 +0300

    added sidenav.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuthenticated = true;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
 }
