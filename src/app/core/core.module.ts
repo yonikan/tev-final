12e7b7276d23c3c01941f7d40cf9de8f987095fd
yoni kangun <yoni.kangun@playermaker.com>
2019-09-03 13:31:09 +0300

    added login & minor  styling.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TeamsComponent } from './teams/teams.component';
 import { LoginComponent } from './login/login.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [TeamsComponent, LoginComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [TeamsComponent, LoginComponent]
 })
--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  isLoading = false;
+  form: FormGroup;
+  username = null;
+  password = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      usernameText: new FormControl(this.username, {
+        validators: [Validators.required, Validators.minLength(5)]
+      }),
+      passwordText: new FormControl(this.password, {
+        validators: [Validators.required, Validators.minLength(5)]
+      })
+    });
   }
 
+  onLogin() {
+    if (!this.form.valid) {
+      return;
+    }
+    this.isLoading = true;
+  }
 }
