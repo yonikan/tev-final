68ddad6c7e8fe3e4aadfcd68373069db79b7103a
yoni kangun <yoni.kangun@playermaker.com>
2019-10-07 22:57:26 +0300

    added more styling.

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { fadeInRightAnimation } from '../core/animations/fade-in-right.animation';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
-  styleUrls: ['./players.component.scss']
+  styleUrls: ['./players.component.scss'],
+  animations: [fadeInRightAnimation]
 })
 export class PlayersComponent implements OnInit {
 
--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInRightAnimation } from '../core/animations/fade-in-right.animation';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
+  animations: [fadeInRightAnimation]
 })
 export class ReportsComponent implements OnInit {
 
