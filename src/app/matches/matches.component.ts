18797deb89059db3bf3a6963606f0b3c31fbf206
yoni kangun <yoni.kangun@playermaker.com>
2019-10-07 22:52:49 +0300

    styling improvments.

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInUpAnimation } from '../core/animations/fade-in-up.animation';
 
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
-  styleUrls: ['./matches.component.scss']
+  styleUrls: ['./matches.component.scss'],
+  animations: [fadeInUpAnimation]
 })
 export class MatchesComponent implements OnInit {
 
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInUpAnimation } from '../core/animations/fade-in-up.animation';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.scss']
+  styleUrls: ['./training.component.scss'],
+  animations: [fadeInUpAnimation]
 })
 export class TrainingComponent implements OnInit {
 
