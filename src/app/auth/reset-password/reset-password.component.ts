f1d995b7ce0952e915464c27fe9889fac265aa03
yoni kangun <yoni.kangun@playermaker.com>
2019-10-16 16:54:15 +0300

    added more translations to the auth.

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -18,8 +18,8 @@ export class ResetPasswordComponent implements OnInit, OnDestroy {
 
   constructor(public authService: AuthService, private formBuilder: FormBuilder) {
     this.passwordFormGroup = this.formBuilder.group({
-      password: ['', Validators.required],
-      repeatPassword: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+      repeatPassword: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]]
     }, {
       validator: RepeatPasswordValidator.validate.bind(this)
     });
