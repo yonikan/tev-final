d8f0e0f8496c2817b3ef77e73d311142bc2a1503
yoni kangun <yoni.kangun@playermaker.com>
2019-10-20 12:01:56 +0300

    added the navigation footer for the mobile.

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  isAuthenticated = false;
+  private authStatusSub: Subscription;
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.isAuthenticated = authStatus;
+      });
   }
 
 }
