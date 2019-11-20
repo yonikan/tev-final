ef0f504fb2650a8e490c732dd5fcad8df4b7055d
yoni kangun <yoni.kangun@playermaker.com>
2019-11-20 15:45:45 +0200

    added bottom-sheet component.

--- src/app/core/services/ui.service.ts
+++ src/app/core/services/ui.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UIService {
-  constructor(private snackbar: MatSnackBar) {}
+  constructor(private snackbar: MatSnackBar, private bottomSheet: MatBottomSheet) {}
 
   showSnackbar(message, action, duration) {
     this.snackbar.open(message, action, {
       duration
     });
+  }
+
+  openBottomSheet(BottomSheetComponent): void {
+    this.bottomSheet.open(BottomSheetComponent);
   }
 }
