53d1fb440bedc517189c0b2466b4e73d889575d8
yoni kangun <yoni.kangun@playermaker.com>
2019-12-17 14:54:02 +0200

    added custom svg icons service.

---
+++ src/app/core/services/custom-icons.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomIconsService {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+  init() {
+    this.matIconRegistry.addSvgIcon(
+      'es',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/svg/flag-es.svg')
+    );
+
+    this.matIconRegistry.addSvgIcon(
+      'us',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/svg/flag-us.svg')
+    );
+  }
+}
