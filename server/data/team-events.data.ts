ded671b10c3971e2e72c780cf35a8b4b27179054
yoni kangun <yoni.kangun@playermaker.com>
2020-01-01 23:58:32 +0200

    minor fix.

--- server/data/team-events.data.ts
+++ server/data/team-events.data.ts
@@ -23,34 +23,24 @@ export const TEAM_EVENTS_DATA: any = {
     ],
     teamEventsValidatedData: [
         {
-            created: 'Yesterday, 08:35-10:3',
-            id: 20,
-            type: 'training',
-            title: '20 new training session',
-            duration: 145,
-            valid_players: 19,
-            phases: 'N/A',
-            isValidated: true
+            id: 23,
+            type: 1,
+            isValidated: true,
+            startTime: 1554250906438,
+            endTime: 1554257103746,
+            validPlayersCount: 12,
+            totalPlayersCount: 22,
+            phasesCount: 4
         },
         {
-            created: 'Yesterday, 08:35-10:3',
-            id: 21,
-            type: 'match',
-            title: '21 new match session',
-            duration: 145,
-            valid_players: 22,
-            phases: 'N/A',
-            isValidated: true
-        },
-        {
-            created: 'Yesterday, 08:35-10:3',
-            id: 22,
-            type: 'training',
-            title: '22new training session',
-            duration: 145,
-            valid_players: 19,
-            phases: 'N/A',
-            isValidated: true
+            id: 24,
+            type: 2,
+            isValidated: true,
+            startTime: 1554250906438,
+            endTime: 1554257103746,
+            validPlayersCount: 16,
+            totalPlayersCount: 22,
+            phasesCount: 2
         }
     ]
 }
