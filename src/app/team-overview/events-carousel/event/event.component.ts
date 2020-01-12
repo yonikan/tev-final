9c45874e824da345472119943b91fdfd1ce5c62f
yoni kangun <yoni.kangun@playermaker.com>
2020-01-12 16:49:22 +0200

    fixes for the offset.

--- src/app/team-overview/events-carousel/event/event.component.ts
+++ src/app/team-overview/events-carousel/event/event.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { enumToString } from 'src/app/core/helpers/helper-functions';
-import { teamEvents } from 'src/app/core/enums/team-events.enum';
+import { enumToString } from '../../../core/helpers/helper-functions';
+import { teamEvents } from '../../../core/enums/team-events.enum';
 import * as moment from 'moment';
 import { ValidatedEventsService } from '../../validated-events/validated-events.service';
-import { offset } from 'highcharts';
 
 @Component({
   selector: 'app-event',
@@ -18,14 +17,12 @@ export class EventComponent implements OnInit {
   @Output() editSessionEmitter = new EventEmitter<any>();
   @Output() deleteSessionEmitter = new EventEmitter<string>();
   @Output() downloadPdfReportEmitter = new EventEmitter<any>();
-
   isValidated = false;
   teamEventTypeString;
   startTimeHoursFormatted;
   endTimeHoursFormatted;
   durationTimeAgo;
   duration;
-
   isPdfReportLoading = false;
   cardTitle: string;
 
@@ -40,9 +37,9 @@ export class EventComponent implements OnInit {
     this.eventData.phaseMaxEndTime = moment(this.eventData.phaseMaxEndTime);
 
     let offset = 0;
-    // if (this.eventData.offset) {
-    //   offset = this.eventData.offset;
-    // }
+    if (this.eventData.offset) {
+      offset = this.eventData.offset;
+    }
 
     this.eventData.startTime = this.eventData.startTime.add(offset, 'hours');
     this.eventData.endTime = this.eventData.endTime.add(offset, 'hours');
@@ -52,8 +49,7 @@ export class EventComponent implements OnInit {
     this.eventData.phaseMaxEndTime = this.eventData.phaseMaxEndTime.add(offset, 'hours');
 
 
-
-    // formatted time
+    // formatted time ===============================================
     let start;
     let end;
     if(this.eventData.type === 1) {
@@ -77,55 +73,7 @@ export class EventComponent implements OnInit {
       end = moment(this.eventData.endTime);
     }
 
-
-
-    // console.log('this.eventData.startTime - ORIGINAL-EPOCH: ', this.eventData.startTime);
-    // this.startTimeHoursFormatted =  this.eventData.startTime;
-    // this.startTimeHoursFormatted = moment(start).format('HH:mm');
-    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
-
-    // this.endTimeHoursFormatted =  this.eventData.endTime;
-    // this.endTimeHoursFormatted = moment(end).format('HH:mm');
-    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
-
-
-    // this.startTimeHoursFormatted = new Date(this.eventData.startTime);
-    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
-    // this.endTimeHoursFormatted = new Date(this.eventData.endTime);
-    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
-    
-
-
-
-    // const dateMoment = moment(this.eventData.startTime);
-    // const hours = dateMoment.hours();
-    // const mins = dateMoment.minutes();
-    // const dateObj = dateMoment.toDate();
-    // dateObj.setHours(hours);
-    // dateObj.setMinutes(mins);
-    // console.log('dateObj: ', dateObj);
-
-
-    // const tmp = moment(this.eventData.startTime).utc(true);
-    // const tmp = new Date(this.eventData.startTime);
-    // let tmpNew = tmp.toISOString();
-    // console.log('tmpNew: ', tmpNew);
-
-    // this.startTimeHoursFormatted = new Date(this.eventData.startTime).toISOString();
-    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
-    // this.endTimeHoursFormatted = new Date(this.eventData.endTime).toISOString();
-    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
-
-
-
-
-
-
-
-
-
-
-    // duration
+    // duration ===============================================
     let duration;
     const startForDuration = moment(this.eventData.startTime);
     const endForDuration = moment(this.eventData.endTime);
@@ -137,46 +85,51 @@ export class EventComponent implements OnInit {
     }
     this.duration = Math.floor(duration);
 
-    
 
-    // team event date
-    // const NOW = moment();
-    // this.durationTimeAgo = NOW.format('dddd, MMMM Do YYYY');
-    // this.durationTimeAgo = '12-12-12';
+    // team event date ===============================================
+    let teamEventStartTime;
+    let teamEventStartEnd;
+    if(this.eventData.type === 1) {
+      if (this.eventData.trimStartTime){
+        teamEventStartTime = this.eventData.trimStartTime;
+        teamEventStartEnd = this.eventData.trimEndTime;
+      } else {
+        teamEventStartTime = this.eventData.startTime;
+        teamEventStartEnd = this.eventData.endTime;
+      }
+    } else if(this.eventData.type === 2) {
+      if (this.eventData.phaseMinStartTime){
+        teamEventStartTime = this.eventData.phaseMinStartTime;
+        teamEventStartEnd = this.eventData.phaseMaxEndTime;
+      } else {
+        teamEventStartTime = this.eventData.startTime;
+        teamEventStartEnd = this.eventData.endTime;
+      }
+    } else {
+      teamEventStartTime = this.eventData.startTime;
+      teamEventStartEnd = this.eventData.endTime;
+    }
 
-    let teamEventDate = moment(this.eventData.startTime);
+    let teamEventDate = moment(teamEventStartTime);
     this.durationTimeAgo = teamEventDate.format('dddd, MMMM Do YYYY');
-
-
-
-
-
-    // const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-    // let current_datetime = new Date(this.eventData.startTime).toISOString();
-    // let date = new Date(current_datetime);
-    // let formatted_date = date.getDate() + "-" + months[date.getMonth()] + "-" + date.getFullYear() + "-" + date.getHours() + "-" + date.getMinutes();
-    // console.log(formatted_date);
-
-    let startHours = new Date(this.eventData.startTime).toISOString().substring(11, 13);
-    let startMinutes = new Date(this.eventData.startTime).toISOString().substring(14, 16);
-    let endHours = new Date(this.eventData.endTime).toISOString().substring(11, 13);
-    let endMinutes = new Date(this.eventData.endTime).toISOString().substring(14, 16);
+    let startHours = new Date(teamEventStartTime).toISOString().substring(11, 13);
+    let startMinutes = new Date(teamEventStartTime).toISOString().substring(14, 16);
+    let endHours = new Date(teamEventStartEnd).toISOString().substring(11, 13);
+    let endMinutes = new Date(teamEventStartEnd).toISOString().substring(14, 16);
     this.startTimeHoursFormatted = `${startHours}:${startMinutes}`;
     this.endTimeHoursFormatted = `${endHours}:${endMinutes}`;
 
-    // this.startTimeHoursFormatted = new Date(this.eventData.startTime).toISOString();
-    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
-    // console.log(moment(this.startTimeHoursFormatted).format("H:mm:ss"));
-    // this.startTimeHoursFormatted =  this.eventData.startTime;
-    // this.startTimeHoursFormatted = moment(start).format('HH:mm');
-    // this.endTimeHoursFormatted = new Date(this.eventData.endTime).toISOString();
-    // this.endTimeHoursFormatted =  this.eventData.endTime;
-    // this.endTimeHoursFormatted = moment(end).format('HH:mm');
+    // let teamEventDate = moment(this.eventData.startTime);
+    // this.durationTimeAgo = teamEventDate.format('dddd, MMMM Do YYYY');
+    // let startHours = new Date(this.eventData.startTime).toISOString().substring(11, 13);
+    // let startMinutes = new Date(this.eventData.startTime).toISOString().substring(14, 16);
+    // let endHours = new Date(this.eventData.endTime).toISOString().substring(11, 13);
+    // let endMinutes = new Date(this.eventData.endTime).toISOString().substring(14, 16);
+    // this.startTimeHoursFormatted = `${startHours}:${startMinutes}`;
+    // this.endTimeHoursFormatted = `${endHours}:${endMinutes}`;
 
 
-
-
-    // card title
+    // card title ===============================================
     this.teamEventTypeString = enumToString(teamEvents, this.eventData.type);
 
     if(this.eventData.isValidated) {
