f2b8772babdf183b0383f0c092915e5cb48ced48
yoni kangun <yoni.kangun@playermaker.com>
2020-01-12 15:05:07 +0200

    fixed offset issue.

--- src/app/team-overview/events-carousel/event/event.component.ts
+++ src/app/team-overview/events-carousel/event/event.component.ts
@@ -32,9 +32,6 @@ export class EventComponent implements OnInit {
   constructor(private validatedEventsService: ValidatedEventsService) { }
 
   ngOnInit() {
-    this.teamEventTypeString = enumToString(teamEvents, this.eventData.type);
-
-    // offset times
     this.eventData.startTime = moment(this.eventData.startTime);
     this.eventData.endTime = moment(this.eventData.endTime);
     this.eventData.trimStartTime = moment(this.eventData.trimStartTime);
@@ -43,9 +40,9 @@ export class EventComponent implements OnInit {
     this.eventData.phaseMaxEndTime = moment(this.eventData.phaseMaxEndTime);
 
     let offset = 0;
-    if (this.eventData.offset) {
-      offset = this.eventData.offset;
-    }
+    // if (this.eventData.offset) {
+    //   offset = this.eventData.offset;
+    // }
 
     this.eventData.startTime = this.eventData.startTime.add(offset, 'hours');
     this.eventData.endTime = this.eventData.endTime.add(offset, 'hours');
@@ -53,6 +50,8 @@ export class EventComponent implements OnInit {
     this.eventData.trimEndTime = this.eventData.trimEndTime.add(offset, 'hours');
     this.eventData.phaseMinStartTime = this.eventData.phaseMinStartTime.add(offset, 'hours');
     this.eventData.phaseMaxEndTime = this.eventData.phaseMaxEndTime.add(offset, 'hours');
+
+
 
     // formatted time
     let start;
@@ -78,23 +77,58 @@ export class EventComponent implements OnInit {
       end = moment(this.eventData.endTime);
     }
 
-    this.startTimeHoursFormatted = moment(start).format('HH:mm');
-    this.endTimeHoursFormatted = moment(end).format('HH:mm');
+
+
+    // console.log('this.eventData.startTime - ORIGINAL-EPOCH: ', this.eventData.startTime);
+    // this.startTimeHoursFormatted =  this.eventData.startTime;
+    // this.startTimeHoursFormatted = moment(start).format('HH:mm');
+    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
+
+    // this.endTimeHoursFormatted =  this.eventData.endTime;
+    // this.endTimeHoursFormatted = moment(end).format('HH:mm');
+    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
+
+
+    // this.startTimeHoursFormatted = new Date(this.eventData.startTime);
+    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
+    // this.endTimeHoursFormatted = new Date(this.eventData.endTime);
+    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
     
+
+
+
+    // const dateMoment = moment(this.eventData.startTime);
+    // const hours = dateMoment.hours();
+    // const mins = dateMoment.minutes();
+    // const dateObj = dateMoment.toDate();
+    // dateObj.setHours(hours);
+    // dateObj.setMinutes(mins);
+    // console.log('dateObj: ', dateObj);
+
+
+    // const tmp = moment(this.eventData.startTime).utc(true);
+    // const tmp = new Date(this.eventData.startTime);
+    // let tmpNew = tmp.toISOString();
+    // console.log('tmpNew: ', tmpNew);
+
+    // this.startTimeHoursFormatted = new Date(this.eventData.startTime).toISOString();
+    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
+    // this.endTimeHoursFormatted = new Date(this.eventData.endTime).toISOString();
+    // console.log('this.endTimeHoursFormatted: ', this.endTimeHoursFormatted);
+
+
+
+
+
+
+
+
+
 
     // duration
     let duration;
     const startForDuration = moment(this.eventData.startTime);
     const endForDuration = moment(this.eventData.endTime);
-    // if(this.eventData.type === 2) {
-    //   if(this.eventData.duration){
-    //     duration = Math.abs(this.eventData.duration);
-    //   } else {
-    //     duration = Math.abs(startForDuration.diff(endForDuration, 'minutes'));
-    //   }
-    // } else {
-    //   duration = Math.abs(startForDuration.diff(endForDuration, 'minutes'));
-    // }
 
     if(this.eventData.duration){
       duration = Math.abs(this.eventData.duration);
@@ -103,74 +137,65 @@ export class EventComponent implements OnInit {
     }
     this.duration = Math.floor(duration);
 
+    
 
+    // team event date
+    // const NOW = moment();
+    // this.durationTimeAgo = NOW.format('dddd, MMMM Do YYYY');
+    // this.durationTimeAgo = '12-12-12';
+
+    let teamEventDate = moment(this.eventData.startTime);
+    this.durationTimeAgo = teamEventDate.format('dddd, MMMM Do YYYY');
+
+
+
+
+
+    // const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    // let current_datetime = new Date(this.eventData.startTime).toISOString();
+    // let date = new Date(current_datetime);
+    // let formatted_date = date.getDate() + "-" + months[date.getMonth()] + "-" + date.getFullYear() + "-" + date.getHours() + "-" + date.getMinutes();
+    // console.log(formatted_date);
+
+    let startHours = new Date(this.eventData.startTime).toISOString().substring(11, 13);
+    let startMinutes = new Date(this.eventData.startTime).toISOString().substring(14, 16);
+    let endHours = new Date(this.eventData.endTime).toISOString().substring(11, 13);
+    let endMinutes = new Date(this.eventData.endTime).toISOString().substring(14, 16);
+    this.startTimeHoursFormatted = `${startHours}:${startMinutes}`;
+    this.endTimeHoursFormatted = `${endHours}:${endMinutes}`;
+
+    // this.startTimeHoursFormatted = new Date(this.eventData.startTime).toISOString();
+    // console.log('this.startTimeHoursFormatted: ', this.startTimeHoursFormatted);
+    // console.log(moment(this.startTimeHoursFormatted).format("H:mm:ss"));
+    // this.startTimeHoursFormatted =  this.eventData.startTime;
+    // this.startTimeHoursFormatted = moment(start).format('HH:mm');
+    // this.endTimeHoursFormatted = new Date(this.eventData.endTime).toISOString();
+    // this.endTimeHoursFormatted =  this.eventData.endTime;
+    // this.endTimeHoursFormatted = moment(end).format('HH:mm');
+
+
+
+
+    // card title
+    this.teamEventTypeString = enumToString(teamEvents, this.eventData.type);
 
     if(this.eventData.isValidated) {
       if(this.eventData.type === 1) {
         if(this.eventData.eventTags) {
-          // console.log('this.eventData.eventTags: ', this.eventData.eventTags);
           this.cardTitle = this.eventData.eventTags[0];
         } else {
           this.cardTitle = `Training Session`;
         }
       } else if(this.eventData.type === 2) {
         if(this.eventData.opponentName) {
-          // console.log('this.eventData.opponentName: ', this.eventData.opponentName);
           this.cardTitle = `Vs. ${this.eventData.opponentName}`;
         } else {
           this.cardTitle = `Match`;
         }
       }
     } else {
-       // const teamEventEnumString = enumToString(teamEvents, this.eventData.type);
        this.cardTitle = `new ${this.teamEventTypeString}`;
     }
-
-
-    const NOW = moment();
-    // const TODAY = NOW.clone().startOf('day');
-    // const YESTERDAY = NOW.clone().subtract(1, 'days').startOf('day');
-
-    // if(NOW.isSame(TODAY, 'd')) {
-    //   this.durationTimeAgo = 'Today';
-    // } else if (NOW.isSame(YESTERDAY, 'd')) {
-    //   this.durationTimeAgo = 'Yesterday';
-    // } else {
-    //   this.durationTimeAgo = NOW.format('dddd, MMMM Do YYYY');
-    // }
-    this.durationTimeAgo = NOW.format('dddd, MMMM Do YYYY');
-
-
-    // ==============================
-    // const now = moment();
-    // const yesterday = moment().add(-1, 'day');
-
-    // if(start.diff(now, 'days') === 0) {
-    //   // console.log('today');
-    //   this.durationTimeAgo = 'today';
-    // } else if (start.diff(yesterday, 'days') === 1) {
-    //   // console.log('yesterday');
-    //   this.durationTimeAgo = 'yesterday';
-    // } else {
-    //   // console.log(now.format('dddd, MMMM Do YYYY'));
-    //   this.durationTimeAgo = now.format('dddd, MMMM Do YYYY');
-    // }
-
-
-    // const startNew = start.endOf('day');
-    // const nowNew = moment().endOf('day');
-    // const yesterdayNew = moment().add(-1, 'day').endOf('day');
-
-    // if(startNew.diff(nowNew, 'days') === 0) {
-    //   // console.log('today');
-    //   this.durationTimeAgo = 'today';
-    // } else if (startNew.diff(yesterdayNew, 'days') === 1) {
-    //   // console.log('yesterday');
-    //   this.durationTimeAgo = 'yesterday';
-    // } else {
-    //   // console.log(now.format('dddd, MMMM Do YYYY'));
-    //   this.durationTimeAgo = nowNew.format('dddd, MMMM Do YYYY');
-    // }
   }
 
   confirmSession() {
