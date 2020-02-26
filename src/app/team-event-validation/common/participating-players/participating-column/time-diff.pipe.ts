6c4e073b34213032f65b48595bbcf67f541f6213
sangress <shai.angress@gmail.com>
2020-02-26 13:41:32 +0200

    fix: PP activity time diff

--- src/app/team-event-validation/common/participating-players/participating-column/time-diff.pipe.ts
+++ src/app/team-event-validation/common/participating-players/participating-column/time-diff.pipe.ts
@@ -1,13 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import moment from "moment-timezone";
+import * as moment from "moment";
 
 @Pipe({
-  name: 'timeDiff'
+	name: 'timeDiff'
 })
 export class TimeDiffPipe implements PipeTransform {
 
-  transform(value: any, endTime?: any, suffix?: string): any {
-    return `${moment(value).diff(moment(endTime), "minutes")}${suffix}`;
-  }
+	transform(value: any, endTime?: any, suffix?: string, roundUp: boolean = true): any {
+		if (roundUp) {
+			const startTime = moment(value).startOf('minute');
+			endTime = moment(endTime).startOf('minute').add(1, 'minute');
+			return `${startTime.diff(endTime, 'minutes')}${suffix}`
+		}
+		return `${moment(value).diff(moment(endTime), "minutes")}${suffix}`;
+	}
 
 }
