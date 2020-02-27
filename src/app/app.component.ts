f528708059c359b1cbb7874aff6ef2d098ef9605
ykangun <yoni.kangun@playermaker.com>
2020-02-27 12:24:42 +0200

    added minor re-implementation for the tags.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -86,7 +86,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
     if (!environment.production) {
       // this.authService.login('ofir.mossinson@playermaker.com', 'Mossinson12345'); // stage
-    //   this.authService.login('yoni.kangun@playermaker.com', 'aaaAAA111'); // dev
+      this.authService.login('yoni.kangun@playermaker.com', 'aaaAAA111'); // dev
       // this.router.navigate(['/team-overview']);
     };
   }
--- src/app/team-event-validation/common/tags/tags.component.ts
+++ src/app/team-event-validation/common/tags/tags.component.ts
@@ -1,32 +1,34 @@
-import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef, OnChanges } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Observable } from 'rxjs';
-import { FormControl } from '@angular/forms';
-import { startWith, map } from 'rxjs/operators';
-import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { TeamEventValidationService } from '../../team-event-validation.service';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.scss']
 })
-export class TagsComponent implements OnInit, OnChanges {
-  @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
-  @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+export class TagsComponent implements OnInit {
   @Input() stepData: any;
   @Input() teamEventType: number;
   @Output() tagsEmitter = new EventEmitter<any>();
-  tagCtrl = new FormControl();
+
   visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  // teamEventTags: any[] = [
+  //   {name: 'Defending'},
+  //   {name: 'Recovery'},
+  //   {name: 'Vertical'},
+  // ];
+
   teamEventTags: any[] = [];
-  filteredTags: Observable<string[]>;
   teamEventTypeString: string;
 
-  constructor() {}
+
+  constructor() { }
 
   ngOnInit() {
     if (this.teamEventType === 1){
@@ -44,21 +46,7 @@ export class TagsComponent implements OnInit, OnChanges {
     }
   }
 
-  ngOnChanges(change) {
-    if (change.stepData && this.stepData) {
-      this.filteredTags = this.tagCtrl.valueChanges
-      .pipe(
-        startWith(null),
-        map((tag: string | null) => {
-          return tag ? this._filter(tag) : this.stepData.availableTagsList.slice();
-        }));
-    }
-  }
-
-  add(event): void {
-    if (this.matAutocomplete.isOpen) {
-      return
-    };
+  add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
@@ -71,29 +59,119 @@ export class TagsComponent implements OnInit, OnChanges {
     if (input) {
       input.value = '';
     }
-    this.sendToTeamEvent(this.teamEventTags);
-  }
-
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.teamEventTags.push({name: event.option.viewValue});
-    this.tagInput.nativeElement.value = '';
-    this.tagCtrl.setValue(null);
   }
 
   remove(teamEventTag: any): void {
     const index = this.teamEventTags.indexOf(teamEventTag);
+
     if (index >= 0) {
       this.teamEventTags.splice(index, 1);
     }
-    this.sendToTeamEvent(this.teamEventTags);
   }
 
   sendToTeamEvent(tags) {
     this.tagsEmitter.emit(tags);
   }
-
-  _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.stepData.availableTagsList.filter(option => option.toLowerCase().includes(filterValue));
-  }
 }
+
+
+
+// import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef, OnChanges } from '@angular/core';
+// import { COMMA, ENTER } from '@angular/cdk/keycodes';
+// import { Observable } from 'rxjs';
+// import { FormControl } from '@angular/forms';
+// import { startWith, map } from 'rxjs/operators';
+// import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+
+// @Component({
+//   selector: 'app-tags',
+//   templateUrl: './tags.component.html',
+//   styleUrls: ['./tags.component.scss']
+// })
+// export class TagsComponent implements OnInit, OnChanges {
+//   @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
+//   @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+//   @Input() stepData: any;
+//   @Input() teamEventType: number;
+//   @Output() tagsEmitter = new EventEmitter<any>();
+//   tagCtrl = new FormControl();
+//   visible = true;
+//   selectable = true;
+//   removable = true;
+//   addOnBlur = true;
+//   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+//   teamEventTags: any[] = [];
+//   filteredTags: Observable<string[]>;
+//   teamEventTypeString: string;
+
+//   constructor() {}
+
+//   ngOnInit() {
+//     if (this.teamEventType === 1){
+//       this.teamEventTypeString = 'training';
+//     } else if (this.teamEventType === 2) {
+//       this.teamEventTypeString = 'match';
+//     } else {
+//       this.teamEventTypeString = 'session';
+//     };
+
+//     if(this.stepData.tags && this.stepData.tags.length > 0) {
+//       this.stepData.tags.forEach(tag => {
+//         this.teamEventTags.push({name: tag})
+//       });
+//     }
+//   }
+
+//   ngOnChanges(change) {
+//     if (change.stepData && this.stepData) {
+//       this.filteredTags = this.tagCtrl.valueChanges
+//       .pipe(
+//         startWith(null),
+//         map((tag: string | null) => {
+//           return tag ? this._filter(tag) : this.stepData.availableTagsList.slice();
+//         }));
+//     }
+//   }
+
+//   add(event): void {
+//     if (this.matAutocomplete.isOpen) {
+//       return
+//     };
+//     const input = event.input;
+//     const value = event.value;
+
+//     // Add our teamEventTag
+//     if ((value || '').trim()) {
+//       this.teamEventTags.push({name: value.trim()});
+//     }
+
+//     // Reset the input value
+//     if (input) {
+//       input.value = '';
+//     }
+//     this.sendToTeamEvent(this.teamEventTags);
+//   }
+
+//   selected(event: MatAutocompleteSelectedEvent): void {
+//     this.teamEventTags.push({name: event.option.viewValue});
+//     this.tagInput.nativeElement.value = '';
+//     this.tagCtrl.setValue(null);
+//   }
+
+//   remove(teamEventTag: any): void {
+//     const index = this.teamEventTags.indexOf(teamEventTag);
+//     if (index >= 0) {
+//       this.teamEventTags.splice(index, 1);
+//     }
+//     this.sendToTeamEvent(this.teamEventTags);
+//   }
+
+//   sendToTeamEvent(tags) {
+//     this.tagsEmitter.emit(tags);
+//   }
+
+//   _filter(value: string): string[] {
+//     const filterValue = value.toLowerCase();
+//     return this.stepData.availableTagsList.filter(option => option.toLowerCase().includes(filterValue));
+//   }
+// }
